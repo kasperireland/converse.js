c8d977c06ee870b6b33cd0ceee7ce8a1c6285b07
Kasper ( git commits by Albert ) <swivrotech@gmail.com>
2021-12-28 22:25:39 -0500

    changes

--- src/plugins/controlbox/templates/navback.js
+++
@@ -1,6 +0,0 @@
-import { html } from "lit";
-import { navigateToControlBox } from '../utils.js';
-
-export default  (jid) => {
-    return html`<i class="fa fa-arrow-left" @click=${() => navigateToControlBox(jid)}></i>`
-}
--- src/templates/form_textarea.js
+++
@@ -1,6 +0,0 @@
-import { html } from "lit";
-
-export default  (o) => html`
-    <label class="label-ta">${o.label}</label>
-    <textarea name="${o.name}">${o.value}</textarea>
-`;
